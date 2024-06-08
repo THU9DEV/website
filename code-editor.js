a55ee2377a18fe87b0cbaf8dd88c534d521325ac
THU9DEV <120350465+THU9DEV@users.noreply.github.com>
2024-06-08 20:31:58 +0700

    Add files via upload

---
+++ code-editor.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const EventEmitter = require('events').EventEmitter;
+
+const EVENT_NAME = "Execute";
+const REQUEST_TIMEOUT = 30000;
+
+const app = express();
+const executeEvent = new EventEmitter();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/executeRequest', (req, res) => {
+  const code = req.body.code;
+
+  if (!code) {
+    return res.sendStatus(500);
+  }
+
+  executeEvent.emit(EVENT_NAME, code);
+  res.sendStatus(200);
+});
+
+app.get('/fetchexecuteRequest', (req, res) => {
+  let timeout;
+  const listener = (code) => {
+    clearTimeout(timeout);
+    res.json({ code: code });
+  };
+
+  executeEvent.once(EVENT_NAME, listener);
+
+  timeout = setTimeout(() => {
+    executeEvent.removeListener(EVENT_NAME, listener);
+    res.sendStatus(500);
+  }, REQUEST_TIMEOUT);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
